5c66d41118f1fc119fa762e8aa158505a1ec0f89
Garrett Wride <garrett.wride@gmail.com>
2024-09-09 10:28:29 -0600

    save as tsv

--- downs_syndrome_metadata.R
+++ downs_syndrome_metadata.R
@@ -1,8 +1,12 @@
 library(tidyverse)
 library(GEOquery)
 
-metadata <- getGEO("GSE101942")[[1]]
+series_id = "GSE101942"
+
+metadata <- getGEO(series_id)[[1]]
 
 metadata = as_tibble(pData(metadata))
 class(metadata)
-view(metadata)  
+view(metadata)
+
+write_tsv(metadata, paste(series_id, "_metadata.tsv", sep=""))
