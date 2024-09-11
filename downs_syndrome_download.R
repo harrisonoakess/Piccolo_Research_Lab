d04239a9f51890cd6e4dfc0f648b23ecd72a8229
Garrett Wride <garrett.wride@gmail.com>
2024-09-11 08:30:28 -0600

    Functions

--- downs_syndrome_download.R
+++ downs_syndrome_download.R
@@ -2,24 +2,22 @@
 
 library(tidyverse)
 
-series_ID = "GSE101942"
+series_ID_new = "GSE101942"
 
-link = paste("https://www.ncbi.nlm.nih.gov/geo/download/?type=rnaseq_counts&acc=", series_ID, "&format=file&file=", series_ID, "_norm_counts_TPM_GRCh38.p13_NCBI.tsv.gz", sep = "")
-link
-# link = "https://www.ncbi.nlm.nih.gov/geo/download/?type=rnaseq_counts&acc=GSE101942&format=file&file=GSE101942_norm_counts_TPM_GRCh38.p13_NCBI.tsv.gz"
-# 
-# link = "https://www.ncbi.nlm.nih.gov/geo/download/?type=rnaseq_counts&acc=GSE114559&format=file&file=GSE114559_norm_counts_TPM_GRCh38.p13_NCBI.tsv.gz"
-# 
-# link = "https://www.ncbi.nlm.nih.gov/geo/download/?type=rnaseq_counts&acc=GSE121065&format=file&file=GSE121065_norm_counts_TPM_GRCh38.p13_NCBI.tsv.gz"
-# ID_pattern = "(GSE)\\d+"
-# series_id = str_extract(link, ID_pattern, group = NULL)
+get_data <- function(series_ID) {
+  link = paste("https://www.ncbi.nlm.nih.gov/geo/download/?type=rnaseq_counts&acc=", series_ID, "&format=file&file=", series_ID, "_norm_counts_TPM_GRCh38.p13_NCBI.tsv.gz", sep = "")
+  
+  # ID_pattern = "(GSE)\\d+"
+  # series_id = str_extract(link, ID_pattern, group = NULL)
+  
+  data <- read_tsv(link)
+  
+  pivot_longer(data, 2:ncol(data), names_to = "sample_id", values_to = "values") %>%
+    pivot_wider(names_from = "GeneID", values_from = "values") -> transposed_data
+  
+  # view(transposed_data)
+  
+  write_tsv(transposed_data, paste(series_ID, "_transposed.tsv", sep=""))
+}
 
-data <- read_tsv(link)
-
-pivot_longer(data, 2:ncol(data), names_to = "sample_id", values_to = "values") %>%
-  pivot_wider(names_from = "GeneID", values_from = "values") -> transposed_data
-
-view(transposed_data)
-
-write_tsv(transposed_data, paste(series_id, "_transposed.tsv", sep=""))
-
+get_data(new_series_ID)
--- downs_syndrome_metadata.R
+++ downs_syndrome_metadata.R
@@ -1,12 +1,15 @@
 library(tidyverse)
 library(GEOquery)
 
-series_id = "GSE101942"
+new_series_ID = "GSE101942"
 
-metadata <- getGEO(series_id)[[1]]
+get_metadata <- function(series_ID) {
+  metadata <- getGEO(series_ID)[[1]]
+  
+  metadata = as_tibble(pData(metadata))
+  # class(metadata)
+  # view(metadata)
+  write_tsv(metadata, paste(series_id, "_metadata.tsv", sep=""))
+}
 
-metadata = as_tibble(pData(metadata))
-class(metadata)
-view(metadata)
-
-write_tsv(metadata, paste(series_id, "_metadata.tsv", sep=""))
+get_metadata(new_series_ID)
