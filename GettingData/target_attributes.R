0761ef67cdc8940b1defa4170de5a54fa5759768
Garrett Wride <garrett.wride@gmail.com>
2024-09-23 10:54:52 -0600

    sex, cell type, geo accession

--- GettingData/target_attributes.R
+++ GettingData/target_attributes.R
@@ -1,12 +1,13 @@
 #----------------------libraries------------------------------------------------
 library(tidyverse)
+library(stringr)
 library(GEOquery)
 
 
 #----------------------data-----------------------------------------------------
-setwd("C:/Users/hoakes/Documents/Piccolo_Research_Lab")
+# setwd("C:/Users/hoakes/Documents/Piccolo_Research_Lab")
   
-RNA_series_ID = c("GSE101942")
+RNA_series_ID = c("GSE128621")
 
 # RNA_series_ID = c("GSE101942", "GSE114559", "GSE121065", "GSE124252", "GSE124513", "GSE126910", "GSE128614", "GSE128621", "GSE131249", "GSE144857", "GSE151282",
 #                   "GSE154418", "GSE160637", "GSE160690", "GSE166849", "GSE167021", "GSE183701", "GSE188568", "GSE190053", "GSE190305", "GSE201093", "GSE203257",
@@ -17,7 +18,7 @@ temp_file_location <- "Data/Metadata/temp_file_target_data.tsv"
 file_location <- "Data/Metadata/"
 
 target_attributes <- tibble (
-  ID = integer(), sex = character(), ploidy = character(), cell_type = character()
+  geo_accession = character(), sex = character(), ploidy = character(), cell_type = character()
   
 )
 # view(target_attributes)
@@ -28,41 +29,52 @@ get_metadata <- function(series_ID, file_location) {
   
   metadata = as_tibble(pData(metadata))
   # view(metadata)
-  print("test")
   
   write_tsv(metadata, file_location) # writes metadata to a temporary file
 }
 
 append_tibble <- function(series_ID, temp_file_location, target_attributes) {
-  # print("test")
   metadata <- read_tsv(temp_file_location)
   metadata_tibble <- as_tibble(metadata)
 
   for (i in 1:nrow(metadata_tibble)){
     row = metadata_tibble[i, ]
-    # view(row)
+    
+    current_sex = "n/a"
+    current_ploidy = "n/a"
+    current_cell_type = "n/a"
+    ID = "n/a"
+    
     column_name <- names(metadata)[grepl("cell type", names(metadata)) |
                                       grepl("cell_type", names(metadata))]
+    # View(column_name)
+    if (length(column_name) != 0) {
+      current_cell_type = row[[column_name]]
+    }
     
+    column_name <- names(metadata)[grepl("geo", names(metadata))]
+    # View(column_name)
+    if (length(column_name) != 0) {
+      print("working")
+      ID = row[[column_name]]
+    }
     
-    current_sex = "Female"
-    current_ploidy = "Disomy"
-    current_cell_type = "stem cells"
+    if (any(str_detect(row, regex("female", ignore_case = TRUE)))){
+      current_sex = "female"
+    } else if (any(str_detect(row, regex("male", ignore_case = TRUE)))){
+      current_sex = "male"
+    }
     
-    target_attributes <- add_row(target_attributes, ID = i, sex = current_sex, ploidy = current_ploidy, cell_type = current_cell_type)
-  
+    target_attributes <- add_row(target_attributes, geo_accession = ID , sex = current_sex, ploidy = current_ploidy, cell_type = current_cell_type)
   }
+  file.remove(temp_file_location) # remove temporary file
   return(target_attributes)
-  # file.remove(temp_file_location) # remove temporary file
 }
 
 process_metadata <- function(series_ID, temp_file_location, target_attributes) {
-  # print("test")
   if (file.exists(temp_file_location)) { # check if temporary file exists
-    # print("test")
     target_attributes = append_tibble(series_ID, temp_file_location, target_attributes) # if it does, drop columns
   } else {
-    # print("test")
     get_metadata(series_ID, temp_file_location) # if it doesn't, get metadata and drop columns
     target_attributes = append_tibble(series_ID, temp_file_location, target_attributes)
   }
