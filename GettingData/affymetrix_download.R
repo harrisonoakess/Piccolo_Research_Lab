8bcf9bb943f3dc2fb25136447ccf60e3679824ce
Garrett Wride <garrett.wride@gmail.com>
2024-10-09 10:56:19 -0600

    affymetrix download

---
+++ GettingData/affymetrix_download.R
@@ -0,0 +1,48 @@
+#--------------------libraries--------------------
+library(GEOquery)
+library(affy)
+# if (!require("BiocManager", quietly = TRUE)) 
+#   install.packages("BiocManager")
+# BiocManager::install("SCAN.UPC")
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("GEOquery")
+
+# getwd()
+
+# setwd("C:\\Users\\garrettwride\\Piccolo_Research_Lab")
+
+#--------------------data--------------------
+# tmpDir =  tempdir()
+# getGEOSuppFiles("GSE110064", makeDirectory = FALSE, baseDir=tmpDir)
+# 
+# celFilePath = file.path(tmpDir, "GSE110064.CEL.gz")
+
+
+#--------------------------------------------------
+
+# Download supplementary files
+getGEOSuppFiles("GSE11877")
+
+# Extract the tar file
+untar("GSE11877/GSE11877_RAW.tar", exdir = "GSE11877_RAW")
+
+# List the extracted files
+list.files("//Data")
+
+# Read the first few lines of the CEL file
+readLines("GSE11877_RAW/GSM11877.CEL.gz", n = 1000)
+
+# Define the full path to your GSE11877_RAW directory
+gse_path <- "C:/Users/garrettwride/Piccolo_Research_Lab/GSE11877_RAW"
+
+# List all the .CEL files in the directory
+cel_files <- list.files(path=gse_path, pattern="^[^.]*\\.CEL\\.gz$")
+
+print(cel_files)
+
+files_to_delete <- setdiff(extracted_files, cel_files)
+
+if (length(files_to_delete) > 0) {
+  file.remove(files_to_delete)
+}
