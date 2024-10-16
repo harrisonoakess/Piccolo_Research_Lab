28849be69122e826fe591fedba9e7c33cf516281
harrisonoakess <oakeshjo00@gmail.com>
2024-10-16 09:49:19 -0600

    updated both files, fixed the lame bug that timed out our file, it is working great now.

--- GettingData/getting_affymatrix_data.R
+++ GettingData/getting_affymatrix_data.R
@@ -32,13 +32,13 @@ options(timeout = max(300, getOption("timeout")))
 # setwd("C:\\Users\\garrettwride\\Piccolo_Research_Lab")
 
 #--------------------data--------------------
-geofiles = c("GSE11877")
+# geofiles = c("GSE11877")
 
-# geofiles = c("GSE110064", "GSE11877", "GSE1281", "GSE1282", "GSE1294", "GSE138861", "GSE1397", "GSE143885", "GSE149459", "GSE149460",
-# "GSE149461", "GSE149462", "GSE149463", "GSE149464", "GSE149465", "GSE158376", "GSE158377", "GSE1611", "GSE16176", "GSE16676",
-# "GSE16677", "GSE168111", "GSE17459", "GSE17760", "GSE1789", "GSE19680", "GSE19681", "GSE19836", "GSE20910", "GSE222355",
-# "GSE30517", "GSE61804", "GSE35561", "GSE35665", "GSE36787", "GSE39159", "GSE4119", "GSE47014", "GSE48611", "GSE49050",
-# "GSE49635", "GSE5390", "GSE59630", "GSE62538", "GSE6283", "GSE65055", "GSE70102", "GSE83449", "GSE84887", "GSE99135")
+geofiles = c("GSE110064", "GSE11877", "GSE1281", "GSE1282", "GSE1294", "GSE138861", "GSE1397", "GSE143885", "GSE149459", "GSE149460",
+"GSE149461", "GSE149462", "GSE149463", "GSE149464", "GSE149465", "GSE158376", "GSE158377", "GSE1611", "GSE16176", "GSE16676",
+"GSE16677", "GSE168111", "GSE17459", "GSE17760", "GSE1789", "GSE19680", "GSE19681", "GSE19836", "GSE20910", "GSE222355",
+"GSE30517", "GSE61804", "GSE35561", "GSE35665", "GSE36787", "GSE39159", "GSE4119", "GSE47014", "GSE48611", "GSE49050",
+"GSE49635", "GSE5390", "GSE59630", "GSE62538", "GSE6283", "GSE65055", "GSE70102", "GSE83449", "GSE84887", "GSE99135")
 
 #------------------functions----------------------
 
@@ -70,9 +70,6 @@ get_scan_upc_files = function(geo_id){
   # List all the .CEL files in the directory
   cel_files <- list.files(path = gse_path, pattern="^[^.]*\\.CEL\\.gz$", full.names= TRUE, ignore.case = TRUE)
   
-  # Make a list of the first 10 files
-  cel_files = c(cel_files[1:10])
-  
   # Makes a list of all files that are not in the above cel_files vector
   files_to_delete <- setdiff(list.files(gse_path, full.names= TRUE), cel_files)
   
@@ -90,7 +87,7 @@ get_scan_upc_files = function(geo_id){
   scan_output_file_f = sprintf("affymetrix_data/%s_SCAN", geo_id)
   
   # last step to converting the information
-  SCAN(celFilePattern, outFilePath = scan_output_file_f, convThreshold = .9, probeLevelOutDirPath = NA)
+  SCAN(celFilePattern, outFilePath = scan_output_file_f, convThreshold = .01, probeLevelOutDirPath = NA)
   
   # Delete the RAW file
   unlink(tar_file_output_f, recursive = TRUE)
