f5d1104aca8ae0e0ab52fc0963cac9aaa691db72
Garrett Wride <garrett.wride@gmail.com>
2024-12-11 10:14:06 -0700

    add permissions to run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -i -t --rm -v "$(pwd)":/my_dir bioc_curation Rscript GettingData/getting_affymetrix_data_docker_file.R
+docker run -i -t --rm -v "$(pwd)":/my_dir --user $(id -u):$(id -g) bioc_curation Rscript GettingData/getting_affymetrix_data_docker_file.R
