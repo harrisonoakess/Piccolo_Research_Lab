187f115eb5ba65ee3726fd94c080920e323985fb
HarrisonOakes <oakeshjo00@gmail.com>
2025-03-12 14:34:25 -0600

    fixed the graphs, and the run statement

---
+++ runQualityGraphs.sh
@@ -0,0 +1,5 @@
+docker run -i -t --rm -v "$(pwd)":/my_dir --user $(id -u):$(id -g) bioc_curation Rscript GettingData/GettingGraphs/QualityGraphs/PlatformQuality
+docker run -i -t --rm -v "$(pwd)":/my_dir --user $(id -u):$(id -g) bioc_curation Rscript GettingData/GettingGraphs/QualityGraphs/GSEQuality
+
+# docker run -i -t --rm -v "$(pwd)":/my_dir --user $(id -u):$(id -g) bioc_curation Rscript GettingData/GettingGraphs/QualityGraphs/GSEQuality && \
+# docker run -i -t --rm -v "$(pwd)":/my_dir --user $(id -u):$(id -g) bioc_curation Rscript GettingData/GettingGraphs/QualityGraphs/PlatformQuality
